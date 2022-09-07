99f6ae13b7716404b97cc61f804ac937f72eb47f
gary909 <garywhite61@hotmail.com>
2022-09-07 13:26:26 +0200

    code working

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 function solution(str, ending) {
-    // TODO: complete
+    str = str.slice(-ending.length);
+    if (str == ending || ending == '') {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(solution('abcde', 'cde')); // true
 console.log(solution('abcde', 'abc')); // false
+console.log(solution('abc', '')); // true
